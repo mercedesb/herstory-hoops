69ef50b88ff4644d40a4355c9a54d1c1cc77af5e
Mercedes Bernard <mercedesrbernard@gmail.com>
2023-03-20 17:55:45 -0500

    update dark mode toggle to preserve state

--- assets/scripts/darkMode.js
+++ assets/scripts/darkMode.js
@@ -1,7 +1,42 @@
+function toggleColorScheme(opts = {}) {
+  document.body.classList.toggle("dark");
+
+  darkInput = document.getElementById("dark-mode");
+  toggle = darkInput.parentElement.querySelector(".toggle");
+
+  if (opts.turnOffAnimation) {
+    // turn off animation so we don't get seasick
+    toggle.classList.add("transition-none");
+  }
+
+  toggle.classList.toggle("checked");
+
+  if (opts.turnOffAnimation) {
+    toggle.classList.remove("transition-none");
+  }
+}
+
 document
   .getElementById("dark-mode")
   .addEventListener("click", function (event) {
-    document.body.classList.toggle("dark");
-    toggle = event.target.parentElement.querySelector(".toggle");
-    toggle.classList.toggle("checked");
+    toggleColorScheme();
+
+    const prefersDark = document.body.classList.contains("dark");
+    localStorage.setItem("prefers-dark", prefersDark);
   });
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const prefersDarkFromStorage = localStorage.getItem("prefers-dark");
+    const prefersDark =
+      prefersDarkFromStorage ||
+      (window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+    if (JSON.parse(prefersDark)) {
+      toggleColorScheme({ turnOffAnimation: true });
+    }
+  },
+  false
+);
