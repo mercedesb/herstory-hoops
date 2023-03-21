b9d8a4364b9de4573fe20a0063adc655a3ad3d80
Mercedes Bernard <mercedesrbernard@gmail.com>
2023-03-21 09:08:33 -0500

    try to fix prod env links

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets/images");
   eleventyConfig.addPassthroughCopy("assets/scripts");
+  return {
+    pathPrefix: process.env.NODE_ENV == "production" ? "/herstory-hoops/" : "",
+  };
 };
