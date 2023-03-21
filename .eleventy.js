647e7a21e2c1dabf3bf093f34c2fa6f40b19e8b9
Mercedes Bernard <mercedesrbernard@gmail.com>
2023-03-21 09:55:10 -0500

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets/images");
   eleventyConfig.addPassthroughCopy("assets/scripts");
+  eleventyConfig.addPassthroughCopy("manifest.json");
   return {
     pathPrefix: process.env.NODE_ENV == "production" ? "/herstory-hoops/" : "",
   };
